64f2ef2d8d87a6df367aebfc5e43f7811ec8c8d6
ramvenkat98 <ramvenkat98@gmail.com>
2024-06-01 16:14:30 -0700

    Compute stats for LLM generations

--- prepare_bar_chart_data_for_finetuning.py
+++ prepare_bar_chart_data_for_finetuning.py
@@ -1,7 +1,8 @@
 import json
 import ast
 
-BAR_CHARTS_ORIGINAL_JSON_FILE_PATH = '/home/ramvenkat98/cs231n-final-project/bar_charts/bar_charts_visual_linguistic_train.json'
+# BAR_CHARTS_ORIGINAL_JSON_FILE_PATH = '/home/ramvenkat98/cs231n-final-project/bar_charts/bar_charts_visual_linguistic_train.json'
+BAR_CHARTS_ORIGINAL_JSON_FILE_PATH = '/Users/ramvenkat98/Desktop/stanford_courses/cs231n/cs231n_final_project/bar_charts/bar_charts_visual_linguistic_train.json'
 BAR_CHARTS_PROCESSED_FILE_PATH = 'bar_charts_processed_visual_linguistic_train.json'
 IMAGE_PATH = '/home/ramvenkat98/cs231n-final-project/'
 BLOCKLIST = [i for i in range(1, 26)] + [
@@ -11,6 +12,8 @@ BLOCKLIST = [i for i in range(1, 26)] + [
 
 with open(BAR_CHARTS_ORIGINAL_JSON_FILE_PATH, 'r') as file:
     data = json.load(file)
+
+STATS_BY_NUM_CHOICES = {}
 
 def generate_formatted_question_and_answer(image, question, choices, answer, id):
     hint = "Hint: Please answer the question and provide the correct option letter, e.g., A, B, C, D, at the end."
@@ -46,6 +49,9 @@ def generate_formatted_question_and_answer(image, question, choices, answer, id)
         print("Choices", choices, type(choices[0]))
         print("Answer", answer, type(answer))
         raise ValueError
+    if len(choices) not in STATS_BY_NUM_CHOICES:
+        STATS_BY_NUM_CHOICES[len(choices)] = [0] * len(choices)
+    STATS_BY_NUM_CHOICES[len(choices)][choices.index(answer)] += 1
     a = f'The answer is ({correct_choice}).'
     return (
         "\n".join([hint, question_formatted, options_formatted]),
@@ -99,7 +105,12 @@ for (i, d) in enumerate(data):
       },
     ]
 
+for k in sorted(STATS_BY_NUM_CHOICES.keys()):
+    print(f"{sum(STATS_BY_NUM_CHOICES[k])} questions with {k} choices. Distribution is {STATS_BY_NUM_CHOICES[k]}.")
+
+'''
 print(len(L), "samples generated.")
 
 with open(BAR_CHARTS_PROCESSED_FILE_PATH, 'w') as file:
     json.dump(L, file, indent = 4)
+'''
