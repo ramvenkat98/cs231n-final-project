9c20b02a55430c60f9b34446acf0f55d0f339fab
ramvenkat98 <ramvenkat98@gmail.com>
2024-06-03 18:53:14 -0700

    Add line chart scripts

---
+++ line_charts/generate_simplified_json_for_inspection.py
@@ -0,0 +1,86 @@
+import json
+import re
+
+with open('line_charts_visual_linguistic_train.json', 'r') as f:
+    L = json.load(f)
+
+'''
+simplified = []
+
+for x in L:
+    simplified.append(
+        {
+            "id": x['id'],
+            "question": x['question'],
+            "answer": x['answer'],
+            "choices": x['choices'],
+        }
+    )
+
+with open('simplified_bar_charts.json', 'w') as f:
+    json.dump(simplified, f, indent = 4)
+'''
+
+# Copied from the IPython notebook
+def extract_code(response):
+    # Look for code blocks delimited by ```python and ```
+    code_pattern = re.compile(r'```python(.*?)```', re.DOTALL)
+    matches = code_pattern.findall(response)
+    # Join all matches into a single string of code
+    return '\n'.join(matches).strip()
+
+# Generate prompts for LLM as judge
+prompts = []
+for x in L:
+    if 'code' not in x:
+        assert('gpt_response' in x)
+        x['code'] = extract_code(x['gpt_response'])
+    prompts.append(f"Question Text:\n{x['question']}\nFigure Code:\n```\n{x['code']}\n```\nChoices:{x['choices']}\nStudent's Response:{x['answer']}\n")
+
+with open('llm_as_judge_prompts.json', 'w') as f:
+    json.dump(prompts, f, indent = 4)
+
+from openai import OpenAI
+client = OpenAI()
+
+corrects, wrongs, unknowns = [], [], []
+
+for i in range(len(prompts)):
+    prompt_messages = [
+        {
+          "role": "system",
+          "content": "You are grading a student's responses on a multiple choice test which requires the student to interpret and analyze visual figures. You are given the textual question and the code to generate the accompanying figure to the question. You are also given the student's response. Please explain your reasoning and grade the student's response as being either correct or wrong. End your answer with either \\boxed{correct} or \\boxed{wrong}.",
+        },
+        {
+          "role": "user",
+          "content": prompts[i]
+        },
+    ]
+    # if L[i]['id'] == 'bar_charts-bar_charts_visual_linguistic-train-31':
+    if i <= 600:
+        print(prompts[i])
+        print(prompt_messages)
+        response = client.chat.completions.create(
+              model="gpt-4",
+              messages=prompt_messages
+        )
+        text = response.choices[0].message.content
+        pattern = r"\\boxed{([^}]*)}"
+        print(text)
+        matches = re.findall(pattern, text)
+        if matches:
+            last_match = matches[-1]  # Get the last item from the list of matches
+            print(last_match)
+        else:
+            last_match = 'unknown'
+        if last_match == 'correct':
+            corrects.append(L[i]['id'])
+        elif last_match == 'wrong':
+            wrongs.append(L[i]['id'])
+        else:
+            unknowns.append(L[i]['id'])
+
+print(corrects, wrongs, unknowns)
+
+with open('llm_as_judge_results.json', 'w') as f:
+    json.dump({"corrects": corrects, "wrongs": wrongs, "unknowns": unknowns}, f, indent = 4)
---
+++ prepare_line_chart_data_for_validation.py
@@ -0,0 +1,119 @@
+import json
+import ast
+import random
+
+randomize_ordering = True
+
+LINE_CHARTS_ORIGINAL_JSON_FILE_PATH = '/home/ramvenkat98/cs231n-final-project/line_charts/line_charts_visual_linguistic_test.json'
+LINE_CHARTS_PROCESSED_FILE_PATH = 'line_charts_processed_visual_linguistic_val_randomized.json'
+IMAGE_PATH = '/home/ramvenkat98/cs231n-final-project/'
+BLOCKLIST = []
+
+with open(LINE_CHARTS_ORIGINAL_JSON_FILE_PATH, 'r') as file:
+    data = json.load(file)
+
+STATS_BY_NUM_CHOICES = {}
+
+def generate_formatted_question_and_answer(image, question, choices, answer, id):
+    hint = "Hint: Please answer the question and provide the correct option letter, e.g., A, B, C, D, at the end."
+    question_formatted = f"Question: {question}"
+    options_formatted = "Choices:"
+    for (i, choice) in enumerate(choices):
+        letter = chr(ord('A') + i)
+        options_formatted += f"\n({letter}) {choice}"
+    try:
+        correct_choice =  chr(ord('A') + choices.index(answer))
+    except:
+        print("Id", id)
+        print("Choices", choices, type(choices[0]))
+        print("Answer", answer, type(answer))
+        raise ValueError
+    if len(choices) not in STATS_BY_NUM_CHOICES:
+        STATS_BY_NUM_CHOICES[len(choices)] = [0] * len(choices)
+    STATS_BY_NUM_CHOICES[len(choices)][choices.index(answer)] += 1
+    a = f'The answer is ({correct_choice}).'
+    return (
+        "\n".join([hint, question_formatted, options_formatted]),
+        a,
+    )
+
+L = []
+already_appeared_ids = set()
+for (i, d) in enumerate(data):
+    image, question, choices, answer = (
+        d['image'],
+        d['question'],
+        d['choices'],
+        d['answer'],
+    )
+    if 'id' in d:
+        # print(d['id'])
+        position_before_number = d['id'].rfind("-")
+        id = int(d['id'][position_before_number + 1 : ])
+        print(id)
+    else:
+        assert(False)
+        id = i
+    if id in BLOCKLIST:
+        print("Id", id, "in blocklist")
+        continue
+    if id in already_appeared_ids:
+        print("Id", id)
+        assert(False)
+    already_appeared_ids.add(id)
+    # Format choices
+    assert(choices is not None) # MCQ only for now
+    if not isinstance(choices, list):
+        assert(isinstance(choices, str))
+        choices = ast.literal_eval(choices)
+    choices = [str(c) for c in choices]
+    if randomize_ordering:
+        random.shuffle(choices)
+    # Format answer
+    if answer[0] == answer[-1] == "'":
+        answer = answer[1:-1]
+    elif answer[0] == answer[-1] =='"':
+        answer = answer[1:-1]
+    elif answer[0] == "'" and answer[-2:] == "'.":
+        answer = answer[1:-2]
+    elif answer[-1] == '.':
+        answer = answer[:-1]
+    elif answer.startswith("Answer: '"):
+        answer = answer[len("Answer: '") : -1]
+    elif answer.startswith('Answer: "'):
+        answer = answer[len('Answer: "') : -1]
+    elif answer.startswith('Answer: '):
+        answer = answer[len('Answer: ') : ]
+    elif answer.startswith("`Answer: "):
+        answer = answer[len("`Answer: ") : -1]
+    image = IMAGE_PATH + image
+    q, a = generate_formatted_question_and_answer(image, question, choices, answer, id)
+    # print(q)
+    # print(a)
+    text = '<ImageHere>' + q
+    L += [
+      {
+        "id": id,
+        "image": [image],
+        "choices": choices,
+        "answer": answer,
+        "conversations": [
+          {
+            "from": "user",
+            "value": text,
+          },
+          {
+            "from": "assistant",
+            "value": a
+          },
+        ]
+      },
+    ]
+
+for k in sorted(STATS_BY_NUM_CHOICES.keys()):
+    print(f"{sum(STATS_BY_NUM_CHOICES[k])} questions with {k} choices. Distribution is {STATS_BY_NUM_CHOICES[k]}.")
+
+print(len(L), "samples generated.")
+
+with open(LINE_CHARTS_PROCESSED_FILE_PATH, 'w') as file:
+    json.dump(L, file, indent = 4)
