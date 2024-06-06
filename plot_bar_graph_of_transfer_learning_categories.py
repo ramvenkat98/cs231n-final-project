dcdc62a6dff6957c6a951ac4cce3ab94cf502e35
Ram Venkat <ramvenkat98@gmail.com>
2024-06-06 01:51:33 +0000

    Add the plotting file (preliminary)

---
+++ plot_bar_graph_of_transfer_learning_categories.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+categories = ['geometry', 'scatter plot', 'table', 'puzzle']
+scores_baseline = [96 / 203, 11 / 16, 6 / 13, 5 / 23]
+scores_train_line_only = [112 / 203, 8 / 16, 7 / 13, 3 / 23]
+scores_train_bar_only = [112 / 203, 7 / 16, 6 / 13, 1 / 23]
+scores_train_line_and_bar = [110 / 203, 8 / 16, 6 / 13, 2 / 23]
